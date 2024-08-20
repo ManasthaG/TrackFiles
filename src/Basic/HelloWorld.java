3694c7c275435e787cde4dd97a3860e64a380918
Manastha <manastha.96@gmail.com>
2024-08-21 00:09:44 +0530

    pull demo

--- src/Basic/HelloWorld.java
+++ src/Basic/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	 System.out.println("Hello there!");
 }
+// demonstrate the pull command
+	public void m1() {
+		System.out.println("From m1");
+	}
 }
